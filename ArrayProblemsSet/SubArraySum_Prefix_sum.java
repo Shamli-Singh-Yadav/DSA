3641b8a83cc543fd52420e558f8422faae272019
Shamli-Singh-Yadav <shamlisinghyadav@gmail.com>
2026-02-04 14:44:31 +0530

    more questions on array

--- ArrayProblemsSet/SubArraySum_Prefix_sum.java
+++ ArrayProblemsSet/SubArraySum_Prefix_sum.java
@@ -1,11 +1,18 @@
+//A prefix array is a separate new array, not the original one.
+//This new array stores the cumulative sum of the original array el  ements.
+
 import java.util.*;
 
-public class Max_Subarray_Sum{
+public class SubArraySum_Prefix_sum{
 
     public static void maxSubarraySum(int numbers[]){
         
         int currSum= 0;
         int maxSum= Integer.MIN_VALUE;
+        int prefix[]= new int[numbers.length];
+
+        //calculate prefix array
+        for(int i=1;)
 
         for (int i=0; i<numbers.length; i++){
             int start = i;
